0e4f174e0cb3a653b1c63e475d75b169561cd0b7
Michael <zamchalin@mail.ru>
2023-10-16 23:40:21 +0300

    Added DZ 5

---
+++ Homework_5/Program.java
@@ -0,0 +1,37 @@
+package ru.geekbrains.lesson5;
+
+import ru.geekbrains.lesson5.models.TableModel;
+import ru.geekbrains.lesson5.presenters.BookingPresenter;
+import ru.geekbrains.lesson5.presenters.Model;
+import ru.geekbrains.lesson5.presenters.View;
+import ru.geekbrains.lesson5.views.BookingView;
+
+import java.util.Date;
+
+public class Program {
+
+    /**
+     * TODO: ДОМАШНЯЯ РАБОТА
+     *  метод changeReservationTable должен заработать!
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Model tableModel = new TableModel();
+        View bookingView = new BookingView();
+        BookingPresenter presenter = new BookingPresenter(tableModel, bookingView);
+        presenter.updateUIShowTables();
+        bookingView.reservationTable(new Date(), 2, "Станислав");
+
+        tableModel.AllReservation();
+
+        ((BookingView) bookingView).changeReservationTable(1001, new Date(), 3, "Станислав");
+        tableModel.AllReservation();
+
+        bookingView.reservationTable(new Date(), 7, "Михаил");
+        tableModel.AllReservation();
+    }
+}
+
+
+
